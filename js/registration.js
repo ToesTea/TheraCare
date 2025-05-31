67e531f562a24da1c052c2e5f165fefc6c41ffe1
mat43987 <matthias.messner@sz-ybbs.ac.at>
2025-05-31 13:43:45 +0200

    About Button-Fixes und Funktion der Buttons wurde hinzugefügt

---
+++ js/registration.js
@@ -0,0 +1,7 @@
+function handleRegistration() {
+    // Zeige die Info-Nachricht
+    alert("Vielen Dank für Ihr Interesse an unserem Workshop! Sie werden gleich zur Anmeldung weitergeleitet. Bitte füllen Sie das Formular auf der nächsten Seite aus und verwenden Sie als Betreff den Namen des Workshops.");
+    
+    // Weiterleitung zur Kontaktseite mit Anker zum Formular
+    window.location.href = 'kontakt.html#anmeldeformular';
+} 

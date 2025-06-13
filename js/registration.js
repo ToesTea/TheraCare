07796d61d140672242fb0cbc2407d2da87fe86a6
mat43987 <matthias.messner@sz-ybbs.ac.at>
2025-06-13 10:30:24 +0200

    Events geändert

--- js/registration.js
+++ js/registration.js
@@ -1,6 +1,6 @@
 function handleRegistration() {
     // Zeige die Info-Nachricht
-    alert("Vielen Dank für Ihr Interesse an unserem Workshop! Sie werden gleich zur Anmeldung weitergeleitet. Bitte füllen Sie das Formular auf der nächsten Seite aus und verwenden Sie als Betreff den Namen des Workshops.");
+    alert("Vielen Dank für Ihr Interesse an unseren Events! Sie werden gleich zur Anmeldung weitergeleitet. Bitte füllen Sie das Formular auf der nächsten Seite aus und verwenden Sie als Betreff den Namen des Events.");
     
     // Weiterleitung zur Kontaktseite mit Anker zum Formular
     window.location.href = 'kontakt.html#anmeldeformular';
